114cec738f897dccb6666ad448d3abd407cc519c
ulisesGutierrez <techwithulises@gmail.com>
2023-01-12 20:17:12 -0600

    metodos

--- javascript/helloWorld.js
+++ javascript/helloWorld.js
@@ -1,6 +1,8 @@
 console.log("hello world");
 
 //Que queremos selecioionar:
+//Tenemos metodos para seleccionar los elementos que tenemos en el html que esta ejecutando a nuestro codigo javascript
+
 const h1 = document.querySelector('h1');
 const p = document.querySelector('p');
 const dos = document.querySelector('.parrafoDos');
@@ -14,3 +16,8 @@ console.log({
     tres,
     input
 });
+// Imprime: <h1>Manipulación del DOM Básica</h1>
+console.log(h1);
+
+//Itroducimos un valor, y colocamos lo siguiente en la consola del navegador, como valor nos dar lo introducido:
+console.log(input.value);
