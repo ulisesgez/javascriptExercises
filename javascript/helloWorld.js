4afb332acecb41a7d976f76c8e0f3705b6cab5e7
ulisesGutierrez <techwithulises@gmail.com>
2023-01-12 20:05:57 -0600

    htmlJavascript

--- javascript/helloWorld.js
+++ javascript/helloWorld.js
@@ -1,1 +1,16 @@
 console.log("hello world");
+
+//Que queremos selecioionar:
+const h1 = document.querySelector('h1');
+const p = document.querySelector('p');
+const dos = document.querySelector('.parrafoDos');
+const tres= document.querySelector('#parrafoTres');
+const input = document.querySelector('input');
+
+console.log({
+    h1,
+    p,
+    dos,
+    tres,
+    input
+});
