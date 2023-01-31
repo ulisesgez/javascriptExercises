cbaad16ab96da785807f7d4dbf8e8ac766ca7f9d
ulisesGutierrez <techwithulises@gmail.com>
2023-01-30 20:03:25 -0600

    exetcises

---
+++ javascript/eliminarDuplicados.js
@@ -0,0 +1,6 @@
+/*
+25) Programa una función que dado un arreglo de elementos,
+elimine los duplicados,
+pe. miFuncion(["x", 10, "x", 2, "10", 10, true, true])
+devolverá ["x", 10, 2, "10", true].
+*/
