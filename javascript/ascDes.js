785381a24b35289907d7c6dbcf24c83667fa0ea7
ulisesGutierrez <techwithulises@gmail.com>
2023-01-30 20:57:14 -0600

    exercises

--- javascript/ascDes.js
+++ javascript/ascDes.js
@@ -4,3 +4,19 @@ con dos arreglos, el primero tendrá los numeros ordenados en forma
 ascendente y el segundo de forma descendiente, pe. miFuncion([7, 5,7,8,6])
 devolverá { asc: [5,6,7,7,8], desc: [8,7,7,6,5] }.
 */
+
+const ordenarArreglo = (arr = undefined) => {
+    if(arr === undefined) return console.warn("No ingresaste un arreglo de numeros");
+    if(!(arr instanceof Array)) return console.error("El valor que ingresaste no es un arreglo");
+    if(arr.length === 0) return console.error("El arreglo esta vacio");
+    for(let num of arr) {
+        if(typeof num !== "number") return console.error("El valor ingresado no es numero");
+    }
+    return console.info({
+        arr,
+        asc: arr.map(el => el).sort(),
+        desc: arr.map(el => el).sort().reverse()
+    });
+}
+
+ordenarArreglo([7, 5,7,8,6]);
--- javascript/eliminarDuplicados.js
+++ javascript/eliminarDuplicados.js
@@ -4,3 +4,19 @@ elimine los duplicados,
 pe. miFuncion(["x", 10, "x", 2, "10", 10, true, true])
 devolverá ["x", 10, 2, "10", true].
 */
+
+const quitarDuplicados = (arr = undefined) => {
+    if(arr === undefined) return console.warn("No ingresaste un arreglo de numeros");
+    if(!(arr instanceof Array)) return console.error("El valor que ingresaste no es un arreglo");
+    if(arr.length === 0) return console.error("El arreglo esta vacio");
+    if(arr.length === 1) return console.error("El arreglo debe tener al menos 2 elementos");
+    for(let num of arr) {
+        if(typeof num !== "number") return console.error("El valor ingresado no es numero");
+    }
+    return console.info({
+        orginal: arr,
+        sinDuplicados: arr.filter((value, index, self) => self.indexOf(value) === index)
+    })
+}
+
+quitarDuplicados([7, 5,7,8,6]);
