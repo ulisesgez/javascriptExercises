3ba1faebbb808521f710811fefef29974d377852
ulisesGutierrez <techwithulises@gmail.com>
2023-03-03 20:38:34 -0600

    operaciones

---
+++ javascript/math.js
@@ -0,0 +1,20 @@
+/*
+const ladoCuadrado = 5;
+const perimetroCuadrado = ladoCuadrado * 4;
+
+console.log(perimetroCuadrado);
+
+const areaCudrada = ladoCuadrado * ladoCuadrado;
+
+console.log(areaCudrada);
+
+ladoTriangulo1 = 6;
+ladoTriangulo2 = 6;
+ladoTrianguloBase = 4;
+alturaTriangulo = 5.5;
+
+const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + ladoTrianguloBase;
+const areaTriangulo = ladoTrianguloBase * alturaTriangulo / 2;
+
+console.log(areaTriangulo);
+*/
